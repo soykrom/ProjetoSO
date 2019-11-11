780830c84a33ce551fd20e5c32779a650acf55bc
JoaoGundersen <soykrom@github.com>
2019-11-11 18:53:17 +0000

    Updated bash script to verify input

--- runTests.sh
+++ runTests.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
-for filename in $1/*.txt; do
-	file=$(basename $filename)
-	IFS='.' read -ra ADDR <<< "$file"
+if [ "$#" -eq 4 ] && [ -d $1 ] && [ -d $2 ] && [ $4 -gt 0 ]; 
 
-	echo InputFile=$file NumThreads=1
-	./tecnicofs-nosync "$filename" "$2/$ADDR-1.txt" 1 1 | grep 'TecnicoFS'
-done
+then
 
-for filename in $1/*.txt; do
-	file=$(basename $filename)
-	IFS='.' read -ra ADDR <<< "$file"
+	for filename in $1/*.txt; 
 	
-	for ((threads = 2; threads <= $3; threads++)); do
-		echo InputFile=$file NumThreads=$threads
+	do
+		file=$(basename $filename)
+		IFS='.' read -ra ADDR <<< "$file"
 
-		./tecnicofs-mutex "$filename" "$2/$ADDR-$threads.txt" $threads $4 | grep 'TecnicoFS'
+		echo InputFile=$file NumThreads=1
+		./tecnicofs-nosync "$filename" "$2/$ADDR-1.txt" 1 1 | grep 'TecnicoFS'
 	done
 
+	if [ 2 -gt $3 ]; 
+	
+	then 
+		exit
+	fi
 
-done
+	for filename in $1/*.txt; 
+	
+	do
+		file=$(basename $filename)
+		IFS='.' read -ra ADDR <<< "$file"
+		
+		for ((threads = 2; threads <= $3; threads++)); 
+		
+		do
+			echo InputFile=$file NumThreads=$threads
+
+			./tecnicofs-mutex "$filename" "$2/$ADDR-$threads.txt" $threads $4 | grep 'TecnicoFS'
+		done
+	done
+fi
